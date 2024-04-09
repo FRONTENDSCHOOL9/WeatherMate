9affa1633b6415ebb09b14f6672721fa605a1af8
holajm <holajjm@naver.com>
2024-04-09 13:54:38 +0900

    ✨ 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import './App.css'
+import Community from "./pages/Community/Community"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import New from "./pages/Community/New"
+
 
 function App() {
-  
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Community />} />
+        <Route path="/new" element={<New />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
 export default App
