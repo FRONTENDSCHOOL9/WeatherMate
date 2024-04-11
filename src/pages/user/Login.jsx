40f1b61d63a385142f9cbfee849faa861da20d48
02.dom <designh2sun@gmail.com>
2024-04-11 16:39:40 +0900

    ✨ 로그인 진행중

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,5 +1,10 @@
+/* eslint-disable */
+import React from 'react'
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>Login hehe</div>
+  )
 }
 
-export default Login;
+export default Login
