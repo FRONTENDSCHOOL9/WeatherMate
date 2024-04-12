cffbca46770dd42d80559dd9b1cc6df9bf70f4f3
woojoung <ywj981217@naver.com>
2024-04-12 09:22:52 +0900

    👔 장소추천 수정

--- src/pages/main/WeatherByTimeZone.jsx
+++ src/pages/main/WeatherByTimeZone.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
 
 function WeatherByTimeZone() {
@@ -45,25 +43,16 @@ function WeatherByTimeZone() {
     <div className="max-w-3xl mx-auto p-4 h-full">
       <h2 className="text-2xl font-bold mb-4">시간대별 기상예보</h2>
       <p className="mb-2">Location: {weatherData.city.name}</p>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={swiper => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        {weatherData.list.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div className="mb-2 flex">
-              <h3 className="font-semibold">{item.dt_txt}</h3>
-              <p>{item.weather[0].description}</p>
-              <p>기온: {(item.main.temp - 273.15).toFixed(1)}°C</p>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+
+      {weatherData.list.map(item => (
+        <>
+          <div className="mb-2 flex">
+            <h3 className="font-semibold">{item.dt_txt}</h3>
+            <p>{item.weather[0].description}</p>
+            <p>기온: {(item.main.temp - 273.15).toFixed(1)}°C</p>
+          </div>
+        </>
+      ))}
     </div>
   );
 }
