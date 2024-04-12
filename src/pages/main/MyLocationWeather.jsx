ba315f08cc75060e15aa06e167fd5b4615f4d01f
woojoung <ywj981217@naver.com>
2024-04-12 11:37:52 +0900

    ⚰️ 장소추천 수정

--- src/pages/main/MyLocationWeather.jsx
+++ src/pages/main/MyLocationWeather.jsx
@@ -6,12 +6,14 @@ import { userWeatherState } from '../../recoil/atom.mjs';
 function MyLocationWeather() {
   const [myPlace, setMyPlace] = useState(''); // 현재 내 위치를 UI에 보여줄 state
   const [userWeather, setUserWeather] = useRecoilState(userWeatherState); // Recoil 상태 및 setter 가져오기
+
   /** 사용자가 웹 페이지에 접속한 현재 위치를 기반으로 날씨를 불러오는 함수 */
   const getUserWeather = async () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async position => {
         const lat = position.coords.latitude; // 사용자 위도
         const lon = position.coords.longitude; //사용자 경도
+
         try {
           const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY; // 공용 KEY
           const response = await axios.get(
@@ -34,28 +36,26 @@ function MyLocationWeather() {
     getUserWeather();
   }, []); // 최초의 마운트 될 시 getUserWeather 호출
 
-  console.log('weather', userWeather);
-
   // 로딩화면
   if (!userWeather) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="text-white w-full pl-9 bg-primary h-[250px]">
-      <h2 className="text-2xl font-bold mb-4">날씨 정보</h2>
-      {/* <img src="/public/02.svg" alt="My Happy SVG" /> */}
-      <h2 className="text-2xl font-bold mb-4">{myPlace.toLocaleUpperCase()}</h2>
+    <div className="text-white w-full pl-9 bg-primary mb-20 mt-20">
+      <img src="/public/02.svg" alt="My Happy SVG" className="mb-3" />
+
       {userWeather && (
         <>
-          <p className="text-lg">
-            온도: {(userWeather.main.temp - 273.15).toFixed(1)}°C
-          </p>
+          <div className="text-4xl font-bold">
+            {(userWeather.main.temp - 273.15).toFixed(1)}°C
+          </div>
           <p className="text-lg font-bold">
             {userWeather.weather[0].description}
           </p>
         </>
       )}
+      <h2 className="text-2xl font-bold mb-4">{myPlace.toLocaleUpperCase()}</h2>
     </div>
   );
 }
