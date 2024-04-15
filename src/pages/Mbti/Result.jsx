62fb2a946104ac1d5dc02bb4e77a700ff4c29fe0
g0meee <kg0meee@gmail.com>
2024-04-15 18:00:11 +0900

    🚧 KakaoShareButton 진행중

--- src/pages/Mbti/Result.jsx
+++ src/pages/Mbti/Result.jsx
@@ -1,14 +1,15 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import MbtiResultData from '@assets/mbti/MbtiResultData';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import KakaoShareButton from '../../components/KakaoShareButton';
 
 function Result() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const mbti = searchParams.get('mbti');
-  const [resultData, setResultData] = React.useState({});
+  const [resultData, setResultData] = useState({});
 
-  React.useEffect(() => {
+  useEffect(() => {
     const result = MbtiResultData.find(s => s.type === mbti);
     setResultData(result);
   }, [mbti]);
@@ -18,7 +19,6 @@ function Result() {
     <div className="m-8 flex gap-4 flex-col items-center">
       <h1 className="text-3xl font-bold">날씨별 성격 테스트</h1>
       <h2>결과 보기</h2>
-      {/* TODO:img, 내용 결과에 따라 다르게 표현되게 바꿔야 됨 */}
 
       <img src={resultData.image} alt="result image" className="md:w-1/2" />
       <button
@@ -27,7 +27,7 @@ function Result() {
       >
         테스트 다시하기
       </button>
-      {/* TODO:카카오톡 공유하기 기능만들기 */}
+      <KakaoShareButton data={MbtiResultData} />
     </div>
   );
 }
