23ddb07fce7f0289aafdda8417012544f5286582
woojoung <ywj981217@naver.com>
2024-04-15 09:37:14 +0900

    ⚰️ 오류수정

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -10,27 +10,25 @@ import WeatherByTimeZone from './WeatherByTimeZone';
 
 function MainWeather() {
   return (
-
     <div className="min-h-[1500px]">
       <div className="bg-primary w-full border-b border-primary rounded-bl-3xl  rounded-br-3xl">
+        <div>
+          <div className="bg-primary w-full min-h-[800px] border-b border-primary rounded-bl-3xl  rounded-br-3xl">
+            <TodaysComent />
+            {/* 오늘의 한마디 */}
+            <MyLocationWeather />
+            {/* 현재 내위치 날씨 */}
+            <WeatherByTimeZone />
+            {/* 시간대 별 날씨 */}
+            <ToTheTopButton />
 
-    <div>
-      <div className="bg-primary w-full min-h-[800px] border-b border-primary rounded-bl-3xl  rounded-br-3xl">
-
-        <TodaysComent />
-        {/* 오늘의 한마디 */}
-        <MyLocationWeather />
-        {/* 현재 내위치 날씨 */}
-        <WeatherByTimeZone />
-        {/* 시간대 별 날씨 */}
-        <ToTheTopButton />
-
-        {/* 최상단으로 이동 버튼  */}
-      </div>
-      <RecommendationPreview />
-      {/* 장소추천 프리뷰  */}
-      <PreviewMbti />
-      {/* mbti test 프리뷰 */}
+            {/* 최상단으로 이동 버튼  */}
+          </div>
+          <RecommendationPreview />
+          {/* 장소추천 프리뷰  */}
+          <PreviewMbti />
+          {/* mbti test 프리뷰 */}
+        </div>
       </div>
     </div>
   );
