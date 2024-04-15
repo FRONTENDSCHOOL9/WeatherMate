8cab684352345c0256f3e41874fa04bc97a70564
02.dom <designh2sun@gmail.com>
2024-04-15 21:31:08 +0900

    🚧 마이페이지 기능 구현

--- src/pages/user/Setting.jsx
+++ src/pages/user/Setting.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 function Setting() {
   return (
+    <>
     <div>Setting</div>
+    <p>개인정보 수정을 하고싶은데 방법을 찾는중..</p>
+    </>
   )
 }
 
--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -14,6 +14,10 @@ function userpage(){
     navigate('/');
   };
 
+  const Edit = () => {
+    console.log({user})
+  };
+
   const [user, setUser] = useRecoilState(memberState);
 
   return (
@@ -30,20 +34,32 @@ function userpage(){
           { user ? (
             <div>
               <p>
-                <img src={`https://market-lion.koyeb.app/api/files/${ user.profile }`}></img>
+                <img className='size-20'
+                src={user.profile ? `https://market-lion.koyeb.app/api/files/${user.profile}` : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'}
+                alt="Profile"
+                />
                 { user.name }님 오늘 날씨 어때요?
                 <Button onClick={ handleLogout }>로그아웃</Button>
               </p>
 
               <ul>
                 <li><Link to="/mbti">MBTI 테스트 하러가기</Link></li>
-                <li><Link to="/boards">자유게시판</Link></li>
-                <li><Link to="/boards">질문게시판</Link></li>
+                <li><Link to="/">저장한 장소</Link></li>{/* 내가 좋아요 누른 게시물 */}
+                <li><Link to="/">나의 활동</Link></li>{/* 내가 좋아요 누른 장소 */}
               </ul>
+
+
+                <Button onClick={() => navigate('/user/Setting')}>설정</Button>
+                <Button onClick={ Edit }>수정</Button>
+
+
             </div>
+
             
           ) : (
             <div>
+              <h2>로그인 후 이용할 수 있어요</h2>
+              <p>원활한 서비스 사용을 위해 로그인을 해주세요!</p>
               <Button onClick={ () => navigate('/user/Login') }>로그인</Button>
               <Button onClick={ () => navigate('/user/SignUp') }>회원가입</Button>
             </div>
@@ -55,4 +71,3 @@ function userpage(){
 }
 
 export default userpage;
-
