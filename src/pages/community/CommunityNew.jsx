4e3b6097db0293989164681ee50de4289bf54c06
holajm <holajjm@naver.com>
2024-04-16 21:51:22 +0900

    💄  날씨 이모티콘 선택 바 생성

--- src/pages/community/CommunityNew.jsx
+++ src/pages/community/CommunityNew.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+// import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import CommunityHeader from "./CommunityHeader";
@@ -6,6 +7,12 @@ import { MdOutlineCameraAlt } from "react-icons/md";
 import useCustomAxios from "@hooks/useCustomAxios.mjs";
 import Button from "../../components/layout/Button";
 import Submit from "../../components/layout/Submit";
+import { TiWeatherSunny } from "react-icons/ti";
+import { TiWeatherDownpour } from "react-icons/ti";
+import { WiCloudy } from "react-icons/wi";
+import { WiCloudyGusts } from "react-icons/wi";
+import { BsCloudSnow } from "react-icons/bs";
+import { WiCloudyWindy } from "react-icons/wi";
 
 function CommunityNew() {
   const {register, handleSubmit, formState: { errors }} = useForm();
@@ -13,33 +20,36 @@ function CommunityNew() {
   const axios = useCustomAxios();
 
   const [image, setImage] = useState(null)
+  // const [click, setClicked] = useState(false);
+
   const [fileName, setFileName] = useState(null);
-  const [weatherClick, setWeatherClick] = useState(false);
-  const [cityClick, setCityClick] = useState(false);
-  const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState(null);
+  // const [weatherClick, setWeatherClick] = useState(false);
+  // const [cityClick, setCityClick] = useState(false);
+  // const [weather, setWeather] = useState(null);
+  // const [city, setCity] = useState(null);
 
-  useEffect(() => {
-    function getPosition(position){
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f24b931da8034c73b91bde1c8d244cad&lang=kr&units=metric`;
-      fetch(API)
-        .then(res => res.json())
-        .then(data => {
-          setCity(data.name);
-          setWeather(data.weather[0].description);
-        })
-    }
-    navigator.geolocation.getCurrentPosition(getPosition)
-  },[city,weather])
+  // useEffect(() => {
+  //   function getPosition(position){
+  //     const lat = position.coords.latitude;
+  //     const lon = position.coords.longitude;
+  //     const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f24b931da8034c73b91bde1c8d244cad&lang=kr&units=metric`;
+  //     fetch(API)
+  //       .then(res => res.json())
+  //       .then(data => {
+  //         setCity(data.name);
+  //         setWeather(data.weather[0].description);
+  //       })
+  //   }
+  //   navigator.geolocation.getCurrentPosition(getPosition)
+  // },[city,weather])
 
-  const handleWeatherClick = () => {
-    setWeatherClick(true)
-  }
-  const handleCityClick = () => {
-    setCityClick(true)
-  }
+  // const handleWeatherClick = () => {
+  //   setWeatherClick(true)
+  // }
+  // const handleCityClick = () => {
+  //   setCityClick(true)
+  // }
+
   const handleFile = (e) => {
     setFileName(e.target.files?.[0].name);
     const file = e.target.files[0];
@@ -48,7 +58,13 @@ function CommunityNew() {
     reader.onload = () => {
       setImage(reader.result);
     }
+    console.log(file);
   }
+
+  // const handleFile = async () => {
+  //   const fileRes = await axios.post('/files')
+  // }
+
   const onSubmit = async (formData) => {
     const res = await axios.post('/posts',formData)
     console.log(res);
@@ -62,42 +78,56 @@ function CommunityNew() {
       <form onSubmit={handleSubmit(onSubmit)}>
 
         <div className="flex">
-          <label htmlFor="file" accept="image/*" className=" w-20 h-20 bg-indigo-200 py-3 mb-4 rounded-lg flex flex-col justify-center items-center text-white">
+          <label htmlFor="file" accept="image/*" className=" w-20 h-20 bg-blue-400 py-3 mb-4 rounded-lg flex flex-col justify-center items-center text-white">
           <MdOutlineCameraAlt className="text-3xl"/>
             사진 추가
             <input onChange={handleFile} type="file" id="file" className="hidden"/>
           </label>
             {image && 
             <div className="flex items-center">
-              <div className="flex flex-col items-center">
-                <img src={image} className=" ml-5 text-md font-bold px-2 w-30 h-16"/>
-                <p>{fileName}</p>
+              <div className="flex items-center">
+                <p className="ml-5">{fileName}</p>
+                <img src={image} className="text-md font-bold px-2 h-8"/>
               </div>
               <Button type="button" onClick={() => setImage()} className="border rounded px-1 ml-2 bg-gray-200 text-white h-8 w-8">X</Button>
             </div>  
             }
         </div>
 
-        <div className="flex justify-start gap-2 mb-2">
+        {/* <div className="flex justify-start gap-2 mb-2">
           {cityClick && <p onClick={() => setCityClick(false)} className="my-1 px-2 text-sm border rounded-lg bg-gray-400 text-white">{city}</p>}
           {weatherClick && <p onClick={() => setWeatherClick(false)} className="my-1 px-2 text-sm border rounded-lg bg-gray-400 text-white text-balance">{weather}</p>}
           <div className="ml-auto">
             <Button type="button" onClick={() => {setCityClick(false),setWeatherClick(false)}} className="border rounded my-1 px-1 bg-gray-200 text-white">지우기</Button>
           </div>
-        </div>
+        </div> */}
 
         <textarea id="content" className="w-full p-4 text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200" 
         autoFocus cols="50" rows="15" placeholder="웨더메이트에게 공유하고 싶은 날씨 이야기를 해주세요!" {...register('content',{required: '내용을 입력하세요.'})}/>
         {errors.content && <p className="ml-1 mb-2 text-bold text-red-500">{errors.content.message}</p>}
 
-        <div className="flex gap-2">
+        {/* <div className="flex gap-2">
           <Button type="button" className="border rounded-lg px-4 py-2 bg-indigo-200 text-indigo-600" onClick={handleWeatherClick}>날씨 추가하기</Button>
           <Button type="button" className="border rounded-lg px-4 py-2 bg-indigo-200 text-indigo-600" onClick={handleCityClick}>위치 추가하기</Button>
+        </div> */}
+
+        <div className="my-4 flex flex-col">
+          <p className="text-lg font-bold">오늘의 날씨 선택</p>
+          <div className="border mt-2 py-2 rounded-xl">
+            <ul className="flex justify-between px-5">
+              <li><TiWeatherSunny className="border rounded-full w-10 h-10 bg-gray-300"/></li>
+              <li><TiWeatherDownpour className="border rounded-full w-10 h-10 p-1 bg-gray-300"/></li>
+              <li><WiCloudy className="border rounded-full w-10 h-10 bg-gray-300"/></li>
+              <li><WiCloudyGusts className="border rounded-full w-10 h-10 bg-gray-300"/></li>
+              <li><BsCloudSnow className="border rounded-full w-10 h-10 p-1 bg-gray-300"/></li>
+              <li><WiCloudyWindy className="border rounded-full w-10 h-10 bg-gray-300"/></li>
+            </ul>
+          </div>
         </div>
 
-        <div className="flex gap-2 float-right">
-          <Button type="button" onClick={() => navigate('/community')} className="p-1 box-border bg-indigo-200 text-indigo-600 rounded-lg">취소</Button>
-          <Submit className="p-1 box-border bg-indigo-200 text-indigo-600 rounded-lg">완료</Submit>
+        <div className="flex gap-2">
+          <Button type="button" onClick={() => navigate('/community')} className="grow p-1 box-border bg-gray-300 text-white rounded-lg">취소</Button>
+          <Submit className="grow p-1 box-border bg-blue-400 text-white rounded-lg">완료</Submit>
         </div>
 
       </form>
