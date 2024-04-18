6df9a8317aebf26f5b92986f4ee92b01e4b33228
woojoung <ywj981217@naver.com>
2024-04-18 16:03:22 +0900

    ✨ 종속성 추가 & 투데이 코멘트 로직 수정

---
+++ src/pages/location/noResultMsg.jsx
@@ -0,0 +1,9 @@
+function NoResultsMessage() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <p className="text-gray-500 text-xl">검색 결과가 없습니다.</p>
+    </div>
+  );
+}
+
+export default NoResultsMessage;
