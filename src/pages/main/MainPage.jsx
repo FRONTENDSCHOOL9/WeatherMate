e2dfce99a45719dafbf6dbce228bfa736bb2c618
woojoung <ywj981217@naver.com>
2024-04-23 20:04:47 +0900

    🐛 API 주소 변경

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -17,25 +17,23 @@ function MainWeather() {
       <div className="w-full">
         <div>
           <div className="w-full ">
-            <div className="flex flex-col xl:flex-row sm:flex-wrap ">
-              <div className="flex flex-col :flex-row lg:flex-wrap ">
-                <div className="sm:w-[50%] ">
-                  <TodaysComent />
-                  <MyLocationWeather />
-                </div>
-                <div className="xl:order-3 w-[100%]">
-                  <WeatherByTimeZone />
-                </div>
-                <div className="sm:w-[50%] sm:mt-40">
-                  <WeatherDetail />
-                  <RecommendationPreview />
-                  <PreviewMbti />
-                </div>
+            <div className="flex flex-col sm:flex-row sm:flex-wrap ">
+              <div className="sm:w-[50%] ">
+                <TodaysComent />
+                <MyLocationWeather />
+              </div>
+              <div className="sm:order-3 w-[100%]">
+                <WeatherByTimeZone />
+              </div>
+              <div className="sm:w-[50%] sm:mt-40">
+                <WeatherDetail />
+                <RecommendationPreview />
+                <PreviewMbti />
               </div>
             </div>
           </div>
-          <ToTheTopButton />
         </div>
+        <ToTheTopButton />
       </div>
     </div>
   );
