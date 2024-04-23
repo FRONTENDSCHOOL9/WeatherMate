5ae7398f3f87332dad34f42ca33e7676f11b4394
g0meee <kg0meee@gmail.com>
2024-04-23 14:07:29 +0900

    🚚 footer->navigationbar

--- src/components/layout/NavigationBarTop.jsx
+++ src/components/layout/NavigationBarTop.jsx
@@ -5,11 +5,11 @@ function NavigationBarTop() {
 
   return (
     <>
-      <footer className="w-full text-white min-h-24 rounded-t-3xl items-center justify-center gap-8 bg-white sticky bottom-0 shadow-inner hidden md:flex">
+      <div className="w-full text-white min-h-24 rounded-t-3xl items-center justify-center gap-8 bg-white sticky bottom-0 shadow-inner hidden md:flex">
         <Link
           to="/main"
           className={`flex flex-col items-center text-black px-2 hover:text-primary ${
-            location.pathname === '/' ? 'text-red-200' : ''
+            location.pathname === '/' ? 'text-primary ' : ''
           }`}
         >
           <p className="text-nowrap">홈</p>
@@ -18,7 +18,7 @@ function NavigationBarTop() {
         <Link
           to="/community"
           className={`flex flex-col items-center px-2 text-black hover:text-primary ${
-            location.pathname === '/community' ? 'text-red-200' : ''
+            location.pathname === '/community' ? 'text-primary ' : ''
           }`}
         >
           <p className="text-nowrap">커뮤니티</p>
@@ -27,7 +27,7 @@ function NavigationBarTop() {
         <Link
           to="/location"
           className={`flex flex-col items-center px-2 text-black hover:text-primary ${
-            location.pathname === '/location' ? 'text-red-200' : ''
+            location.pathname === '/location' ? 'text-primary ' : ''
           }`}
         >
           <p className="text-nowrap">장소추천</p>
@@ -36,12 +36,12 @@ function NavigationBarTop() {
         <Link
           to="/user/mypage"
           className={`flex flex-col items-center px-2 text-black hover:text-primary ${
-            location.pathname === '/user/mypage' ? 'text-red-200' : ''
+            location.pathname === '/user/mypage' ? 'text-primary ' : ''
           }`}
         >
           <p className="text-nowrap">마이페이지</p>
         </Link>
-      </footer>
+      </div>
     </>
   );
 }
--- src/components/layout/NavigationBarbottom.jsx
+++ src/components/layout/NavigationBarbottom.jsx
@@ -10,7 +10,9 @@ function NavigationBarBottom() {
       <footer className="w-full flex text-white min-h-24 rounded-t-3xl items-center justify-center gap-8 bg-white sticky bottom-0 shadow-inner sm:hidden">
         <Link
           to="/main"
-          className="flex flex-col items-center text-primary px-2"
+          className={`flex flex-col items-center text-black px-2 hover:text-primary ${
+            location.pathname === '/' ? 'text-primary ' : ''
+          }`}
         >
           <GoHomeFill className="text-4xl mb-1" />
           <p className="text-nowrap">홈</p>
@@ -18,7 +20,9 @@ function NavigationBarBottom() {
 
         <Link
           to="/community"
-          className="flex flex-col items-center px-2 text-primary"
+          className={`flex flex-col items-center px-2 text-black hover:text-primary ${
+            location.pathname === '/community' ? 'text-primary ' : ''
+          }`}
         >
           <SiGooglechat className="text-4xl mb-1" />
           <p className="text-nowrap">커뮤니티</p>
@@ -26,7 +30,9 @@ function NavigationBarBottom() {
 
         <Link
           to="/location"
-          className="flex flex-col items-center px-2 text-primary"
+          className={`flex flex-col items-center px-2 text-black hover:text-primary ${
+            location.pathname === '/location' ? 'text-primary ' : ''
+          }`}
         >
           <MdLocationOn className="text-4xl mb-1" />
           <p className="text-nowrap">장소추천</p>
@@ -34,7 +40,9 @@ function NavigationBarBottom() {
 
         <Link
           to="/user/mypage"
-          className="flex flex-col items-center px-2 text-primary"
+          className={`flex flex-col items-center px-2 text-black hover:text-primary ${
+            location.pathname === '/location' ? 'text-primary ' : ''
+          }`}
         >
           <FaAddressBook className="text-4xl mb-1" />
           <p className="text-nowrap">마이페이지</p>
