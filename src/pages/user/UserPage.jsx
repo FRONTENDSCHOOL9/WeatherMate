300afb7f9a60c120f29c8fcbca9f3dd30ada1b45
02.dom <designh2sun@gmail.com>
2024-04-25 00:45:44 +0900

    🚧 userpage 수정

--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,57 +1,33 @@
 /* eslint-disable */
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { memberState } from '@recoil/atom.mjs';
-import { Link } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import Button from '@components/layout/Button';
 import LocationBookMark from '@pages/location/LocationBookmark';
-//여기도
 import { useQuery } from '@tanstack/react-query';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
-import CommunityItem from '@pages/community/CommunityItem';
 import UserBoard from '@pages/user/UserBoard';
-//여기까지 바뀜
 
 function UserPage() {
   const navigate = useNavigate();
   const [user, setUser] = useRecoilState(memberState);
-  
+  const [click, setClick] = useState(false);
+  const axios = useCustomAxios();
 
   const handleLogout = () => {
     setUser(null);
     navigate('/');
   };
 
-
-  // const redirect_uri = `${window.location.origin}/auth/kakao`; //Redirect URI
-  // const REST_API_KEY=import.meta.env.VITE_KAKAO_REST_API_KEY;
-  
-
-  //인가 코드를 받고
-
-
-  //우리 api 서버에 보내주기
-  //api 서버가 카카오에 연결이 돼서 정보를 받아온다
-
-  
-  // oauth 요청 URL
-  // const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${redirect_uri}&response_type=code`;
-
   const handleLogin = () => {
-    // console.log(REST_API_KEY, kakaoURL)
-    // window.location.href = kakaoURL;
+    // handle login logic here
   };
-
 
   const Edit = () => {
     navigate('/user/edit');
   };
 
-
-  //여기부터
-  const [click, setClick] = useState(false);
-  const axios = useCustomAxios();
   const { data } = useQuery({
     queryKey: ['posts'],
     queryFn: () =>
@@ -60,21 +36,13 @@ function UserPage() {
           type: "community"
         },
       }),
-      select: (response) => response.data,
-      suspense: true,
-      refetchOnMount: "always"
+    select: (response) => response.data,
+    suspense: true,
+    refetchOnMount: "always"
   });
-  console.log(user);
-  console.log(data);
-  const itemList = data?.item?.filter((item) =>{
-    if(user){
-      if(item.user._id === user._id){
-        return item
-      }
-    }
-  })
-  .map((item) => <UserBoard key={item._id} item={item} />);
-  //여기까지 바뀜
+
+  const itemList = data?.item?.filter((item) => item.user._id === user._id)
+    .map((item) => <UserBoard key={item._id} item={item} />);
 
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center">
@@ -84,10 +52,7 @@ function UserPage() {
             <div className="flex items-center mb-4">
               <img
                 className="w-10 h-10 rounded-full mr-4"
-                src={
-                  user.profile?.startsWith('http') ?
-                  user.profile : `${import.meta.env.VITE_API_SERVER}/files/07-WeatherMate/${user.profile}`
-                }
+                src={user.profile ? user.profile : '/nulluser.svg'}
                 alt="Profile"
               />
               <p className="text-lg font-semibold">{user.name}님 오늘 날씨 어때요?</p>
@@ -97,19 +62,16 @@ function UserPage() {
                 <li className="mb-2 bg-slate-200">
                   <Link to="/mbti" className="text-blue-500 hover:underline">MBTI 테스트 하러가기</Link>
                 </li>
-                
                 <li className="mb-2">
                   <Link to="/" className="text-blue-500 hover:underline">저장한 장소</Link>
                 </li>
                 <li>
-                  {/* 여기부터 */}
                   <div>
                     <button type="button" onClick={() => setClick(true)} className="text-blue-500 hover:underline">나의 활동</button>
                     <div className="flex flex-col gap-2">
                       {click && itemList}
                     </div>
                   </div>
-                  {/* 여기까지 바뀜 */}
                 </li>
               </ul>
             </div>
@@ -120,13 +82,11 @@ function UserPage() {
             </div>
             <LocationBookMark />
           </div>
-
         ) : (
           <div className="text-center">
             <h2 className="text-3xl font-black text-primary_deep mb-4 pt-6 font-Ainmom">로그인 후 이용할 수 있어요</h2>
             <p className="mb-4 text-gray-600">원활한 서비스 사용을 위해 로그인을 해주세요!</p>
-          <img className='w-[50%] m-auto comment-float pt-2 pb-10' src='/mainlogin.svg' />
-
+            <img className='w-[50%] m-auto comment-float pt-2 pb-10' src='/mainlogin.svg' />
             <div className="mb-4 w-full flex flex-col gap-4 comment-text">
               <button
                 className="bg-primary text-white py-2 px-4 rounded-lg mr-2 hover:bg-primary_deep"
@@ -146,7 +106,6 @@ function UserPage() {
               >
                 카카오로 시작하기
               </button>
-
             </div>
             <Link to="/" className="text-gray-500 hover:underline text-sm">웨더메이트 둘러보기</Link>
           </div>
